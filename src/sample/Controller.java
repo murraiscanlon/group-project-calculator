f6a442af466b29f9360e65d72b5e8452e76c03d7
Murrai Scanlon <50279093+murraiscanlon@users.noreply.github.com>
2021-01-31 13:25:51 -0600

    calculate button is accurately calculating final average and setting it to the output label (average label)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,39 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
-    public void calculate_grade_on_mouse_click(){
-        System.out.println("The Calculate button has been clicked!");
+public class Controller {
+    @FXML
+    public Button calculate_button;
+    @FXML
+    TextField product_grade_textfield;
+    @FXML
+    TextField participation_grade_textfield;
+    @FXML
+    TextField submission_grade_textfield;
+    @FXML
+    Label average_label;
+
+    public void calculate_grade_on_mouse_click(ActionEvent event){
+
+        //average_label.setText(product_grade_textfield.getText());
+
+        int productGrade = Integer.parseInt(product_grade_textfield.getText());
+        int participationGrade = Integer.parseInt(participation_grade_textfield.getText());
+        int submissionGrade = Integer.parseInt(submission_grade_textfield.getText());
+
+        double finalAverage = (double) productGrade * .45;
+        finalAverage += (double) participationGrade * .45;
+        finalAverage += (double) submissionGrade * .1;
+
+
+        average_label.setText(String.format("%.2f",finalAverage));
+
     }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("calculator_interface.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Group Project Calculator");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
     }
