85c5276c399c48cc5a27918fd1c3b47049d0ef79
Murrai Scanlon <50279093+murraiscanlon@users.noreply.github.com>
2021-01-31 19:07:37 -0600

    save() is working. Still need to create print()

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,12 +7,25 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
+import static java.lang.System.currentTimeMillis;
+
 
 public class Controller {
+
+    SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy [hh:mm]");
+    Date date = new Date();
+    double finalAverage;
+    Projects projects = new Projects();
+    //FileWriter myWriter;
+
+
 
     /*** TextFields  ***/
     @FXML
@@ -45,17 +58,18 @@ public class Controller {
 
 
     /* Calculates the project weighted average based on input by user*/
-    public void calculate_grade_on_mouse_click(ActionEvent event){
+    public double calculate_grade_on_mouse_click(ActionEvent event){
         int productGrade = Integer.parseInt(product_grade_textfield.getText());
         int participationGrade = Integer.parseInt(participation_grade_textfield.getText());
         int submissionGrade = Integer.parseInt(submission_grade_textfield.getText());
 
-        double finalAverage = (double) productGrade * .45;
+        finalAverage = (double) productGrade * .45;
         finalAverage += (double) participationGrade * .45;
         finalAverage += (double) submissionGrade * .1;
 
         average_label_output.setText(String.format("%.2f",finalAverage));
 
+        return finalAverage;
     }
 
     public void clear(ActionEvent event){
@@ -78,10 +92,40 @@ public class Controller {
 
     public void save(){
 
-        /*** Confirmation Message ***/
-        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy [hh:mm]");
-        Date date = new Date();
+        //Confirmation Message
         display_label.setText("Successfully Saved   ------   " + sdf.format(date.getTime()));
+
+        //Add textfields data to Objects/class - - - print later
+        projects.setGroupNumber(group_number_textfield.getText());
+        projects.setProductGrade(product_grade_textfield.getText());
+        projects.setParticipationGrade(participation_grade_textfield.getText());
+        projects.setSubmissionGrade(submission_grade_textfield.getText());
+        projects.setFinalGrade(finalAverage);
     }
 
 }
+
+/*
+        //Write to File
+        try {
+
+                //if (myWriter == null) {
+                FileWriter myWriter = new FileWriter("C:/Users/Murrai.Scanlon/Desktop/project_grades/"
+                + projectname_textfield.getText() + "_"
+                + classname_textfield.getText() + "_"
+                + section_textfield.getText() + ".txt");
+                myWriter.write("\n" + sdf.format(date.getTime()) + "\n");
+                myWriter.write("\n");
+                //}
+
+                for (int i = 0; i < listOfGrades.size(); i++){
+        myWriter.write(listOfGrades.get(i));
+        }
+        myWriter.close();
+        System.out.println("--------------------");
+        System.out.println("\nYour file has been successfully written!\n");
+        } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+        }
+ */
